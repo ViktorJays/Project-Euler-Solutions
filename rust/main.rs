5dd017509cf1a5f429fc3add23ce3ab81e02a17a
VJ <d-thenifts5_nc_th@outlook.com>
2021-05-11 16:49:09 +0100

    exercicio 2

--- rust/main.rs
+++ rust/main.rs
@@ -1,5 +1,6 @@
 fn main() {
-    multiples();
+    //multiples();
+    fibonacci_even();
 }
 
 // Exercicio 1
@@ -12,6 +13,42 @@ fn multiples() {
         }
     }
 
-    return print!("{}", rs);
+    return print!("Multiples of 3 or 5 bellow 1000 sum:{}", rs);
 }
 
+// Exercicio 2
+fn fibonacci(mut nth: i32) -> i32 {
+    /*let mut n1 = 1;
+    let mut n2 = 2;
+    let mut rs = 0;
+    print!("{},{}",n1,n2);
+    for _ in 0..(nth-2) {
+        rs = n2 + n1;
+        n1 = n2;
+        n2 = rs;
+        print!(",{}",rs);
+    }*/
+    return if nth == 0 {
+        nth
+    } else if nth == 1 {
+        nth
+    } else {
+        fibonacci(nth - 2) + fibonacci(nth - 1)
+    };
+}
+
+fn fibonacci_even() {
+    let mut aux = 1;
+    let mut even_sum = 0;
+    loop {
+        if fibonacci(aux) > 4000000 {
+            break;
+        }
+        if fibonacci(aux) % 2 == 0 {
+            println!("[{}]", fibonacci(aux));
+            even_sum += fibonacci(aux);
+        }
+        aux += 1;
+    }
+    print!("Even-Valued Terms lesser than 4 million sum: {}", even_sum);
+}
