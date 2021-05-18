6170230c7992c590f67ea2640cbcf5eff7407684
VJ <d-thenifts5_nc_th@outlook.com>
2021-05-18 17:17:03 +0100

    exercicio 3 incompleto . melhorias no codigo

--- rust/exec_3.rs
+++ rust/exec_3.rs
@@ -1,12 +1,13 @@
 pub(crate) fn prime_n() {
     let mut flag = 0;
     let mut loop_num = 1;
-    let prime_n = 5;
-    let loop_limit = 10;
+    let prime_sequence = 1000;
+    let loop_limit = prime_sequence;
     let mut p_n = 1;
+    let mut counter = 1;
 
     loop {
-        if p_n > prime_n {
+        if p_n > prime_sequence {
             break;
         } else {
             //println!("Q: {} is prime? x loop: {}", p_n, loop_num);
@@ -18,12 +19,13 @@ pub(crate) fn prime_n() {
                     loop_num += 1;
                 }
             } else if flag <= 2 {
-                println!("A: {} IS prime!", p_n);
+                println!("{}: {} IS prime!", counter, p_n);
                 p_n += 1;
+                counter += 1;
                 loop_num = 1;
                 flag = 0;
             } else {
-                println!("A: {} is NOT prime!", p_n);
+                //println!("A: {} is NOT prime!", p_n);
                 p_n += 1;
                 loop_num = 1;
                 flag = 0;
